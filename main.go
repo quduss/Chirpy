2e19654a759e4787886775cb28b3884f9de85483
quduss <shuaib.qudus@yahoo.com>
2025-08-18 11:44:15 +0100

    Create a middle ware method that increments fileserverHits and calls the next handler

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. Write the Content-Type header
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
