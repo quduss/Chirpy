1e4d579f126f5c10cc375c8bc810bc7bb7a000bf
quduss <qudshuaib@gmail.com>
2025-09-08 15:45:53 +0100

    Handle POST /api/chirps in the main function

--- main.go
+++ main.go
@@ -309,6 +309,8 @@ func main() {
 
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
+	mux.HandleFunc("POST /api/chirps", apiCfg.createChirpHandler)
+
 	mux.HandleFunc("POST /api/users", apiCfg.createUserHandler)
 
 	server := &http.Server{
