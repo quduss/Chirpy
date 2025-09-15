fea89f704fe9b10afd6fa46681dd295fbb3a3433
quduss <qudshuaib@gmail.com>
2025-09-15 13:19:54 +0100

    Create function that validates a JWT token and returns the user ID

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -25,3 +25,24 @@ func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (str
 
 	return tokenString, nil
 }
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if !ok {
+		return uuid.UUID{}, jwt.ErrInvalidKey
+	}
+
+	userID, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	return userID, nil
+}
