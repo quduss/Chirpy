a116f14a4ee45c4fde14424339bd8aee4e3ae905
quduss <qudshuaib@gmail.com>
2025-09-18 20:05:38 +0100

    Write query that retrieves user from a refresh token

--- sql/queries/tokens.sql
+++ sql/queries/tokens.sql
@@ -2,3 +2,10 @@
 INSERT INTO refresh_tokens (token, user_id, expires_at)
 VALUES ($1, $2, $3)
 RETURNING *;
+
+-- name: GetUserFromRefreshToken :one
+SELECT u.* FROM users u
+JOIN refresh_tokens rt ON rt.user_id = u.id
+WHERE rt.token = $1 
+  AND rt.expires_at > NOW()
+  AND rt.revoked_at IS NULL;
