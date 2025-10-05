1deb5eed01ec7bf30dcc2cf5f5c1751adae9f9df
quduss <qudshuaib@gmail.com>
2025-10-05 22:53:04 +0100

    Write query that revokes a refresh token

--- sql/queries/tokens.sql
+++ sql/queries/tokens.sql
@@ -9,3 +9,11 @@ JOIN refresh_tokens rt ON rt.user_id = u.id
 WHERE rt.token = $1 
   AND rt.expires_at > NOW()
   AND rt.revoked_at IS NULL;
+
+-- name: RevokeRefreshToken :exec
+-- Revokes a refresh token by setting revoked_at and updated_at
+-- This effectively "logs out" the user from this session
+UPDATE refresh_tokens 
+SET revoked_at = NOW(), 
+    updated_at = NOW()
+WHERE token = $1;
