9880c0018c7088e5d6a8b7b11b55506617d78304
quduss <qudshuaib@gmail.com>
2025-10-05 21:58:27 +0100

    Generate method GetUserFromRefreshToken

--- internal/database/tokens.sql.go
+++ internal/database/tokens.sql.go
@@ -37,3 +37,24 @@ func (q *Queries) CreateRefreshToken(ctx context.Context, arg CreateRefreshToken
 	)
 	return i, err
 }
+
+const getUserFromRefreshToken = `-- name: GetUserFromRefreshToken :one
+SELECT u.id, u.created_at, u.updated_at, u.email, u.hashed_password FROM users u
+JOIN refresh_tokens rt ON rt.user_id = u.id
+WHERE rt.token = $1 
+  AND rt.expires_at > NOW()
+  AND rt.revoked_at IS NULL
+`
+
+func (q *Queries) GetUserFromRefreshToken(ctx context.Context, token string) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUserFromRefreshToken, token)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Email,
+		&i.HashedPassword,
+	)
+	return i, err
+}
